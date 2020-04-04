f383899424b2d456199af9a32b800a151a23d2a4
menynexi <meny_nexi@outlook.com>
2020-04-03 18:38:01 -0600

    I a done with this lab mentally

--- src/AminoAcidResourcesTester.java
+++ src/AminoAcidResourcesTester.java
@@ -59,6 +59,83 @@ class AminoAcidResourcesTester{
     }
   }
 
+  //these first two test cases should translate the string to a linklist and return the correct charatcer array
+  @Test
+  public void test1(){//uppercase as it should wolk
+    char[] e = {'A','T','G','L','A'};
+    AminoAcidLL a = AminoAcidLL.createFromRNASequence("GCUACGGAGCUUCGGAGCUAG");
+    assertArrayEquals(e, a.aminoAcidList());
+  }
+
+  @Test
+  public void test2(){//lower case
+    char[] e = {'A','T','G','L','A'};
+    AminoAcidLL a = AminoAcidLL.createFromRNASequence("GCUACGGAGCUUCGGAGCUAG");
+    assertArrayEquals(e, a.aminoAcidList());
+  }
+
+  //these two test cases should test if the list is sorted or if it needs to be sorted one test case shsould be true and the other should be
+  @Test
+  public void test3(){//this list is not sorted
+    AminoAcidLL a = AminoAcidLL.createFromRNASequence("GCUACGGAGCUUCGGAGCUAG");
+    assertEquals(false, a.isSorted());
+  }
+
+  @Test
+  public void test4(){//This list is sorted
+    AminoAcidLL a = AminoAcidLL.createFromRNASequence("GCGGACUAG");
+    assertEquals(true, a.isSorted());
+  }
+
+  @Test
+  public void test5(){//this is going to test if the method sort was implemented correctly with an unsorted list
+    AminoAcidLL a = AminoAcidLL.createFromRNASequence("GCUACGGAGCUUCGGAGCUAG");
+    a = AminoAcidLL.sort(a);
+    assertEquals(true, a.isSorted());
+  }
+
+  @Test
+  public void test6(){//this is going to test if the method sort is implemented correctly with an already sorted list
+    AminoAcidLL a = AminoAcidLL.createFromRNASequence("GCGGACUAG");
+    a = AminoAcidLL.sort(a);
+    assertEquals(true, a.isSorted());
+  }
+
+  @Test
+  public void test7(){//this count the amino acids
+    int[] e = {2,1,1,1,1};
+    AminoAcidLL a = AminoAcidLL.createFromRNASequence("GCUACGGAGCUUCGGAGCUAG");
+    assertEquals(e, a.aminoAcidCounts());
+  }
+
+  @Test
+  public void test8(){// counts the amino acids
+    int[] e = {1,1,1,1,1};
+    AminoAcidLL a = AminoAcidLL.createFromRNASequence("ACGGAGCUUCGGAGCUAG");
+    assertEquals(e, a.aminoAcidCounts());
+  }
+
+  @Test
+  public void test9(){//does it count a stop codon
+    int[] e = {0};
+    AminoAcidLL a = AminoAcidLL.createFromRNASequence("UAG");
+    assertEquals(e, a.aminoAcidCounts());
+  }
+
+  @Test// I tried this last method after talking to michell as to how to compare
+  public void test10(){
+    String str = "ATGLAS";
+    AminoAcidLL a = AminoAcidLL.createFromRNASequence("GCUACGGAGCUUCGGAGCUAG");
+    for(int i = 0; i < str.length() - 1; i++){
+      assertEquals(str.charAt(i), a.aminoAcid);
+      a = a.next;
+    }
+  }
+
+
+
+
+
 
 
 
